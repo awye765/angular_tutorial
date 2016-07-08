51e79e5a6d2b787b9cc32fe9d21b8c926d1d31dc
awye765 <awye765@googlemail.com>
2016-07-08 11:29:48 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+(function(){
+  var app = angular.module('gemStore', []);
+// Creates new variable.  Assigns it a new Angular module, with application
+// name, "store", that takes an empty array, which will be used for dependency
+// injection of any other libraries we might need.
+  app.controller('StoreController', function() {
+// The above is the controller and is attached to (INSIDE) our app.
+    this.products = gems;
+// The above sets the gem equal to a property of our controller, in this case,
+// "products".
+  });
+
+  var gems = [
+    {
+      name: 'Dodecahedron',
+      price: 2.95,
+      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
+      canPurchase: true,
+      soldOut: false,
+    },
+    {
+      name: 'Pentagonal Gem',
+      price: 5.95,
+      description: ". . .",
+      canPurchase: true,
+      soldOut: false,
+    }
+  ];
+// The above stores the gem variable and its data INSIDE the app.
+})();
---
+++ notes/angular_notes.js
@@ -0,0 +1,36 @@
+// WHAT IS ANGULAR?
+//
+// Angular is a CLIENT-SIDE JAVASCRIPT FRAMEWORK for adding INTERACTIVITY to HTML.
+// Client-side means referring to operations that are performed by the CLIENT (on
+// the CLIENT'S COMPUTER) in a CLIENT-SERVER relationship in a computer network.
+//
+// HOW DO WE TELL HTML TO TRIGGER THE JAVASCRIPT?
+//
+// In Angular we add behaviour to HTML by using a DIRECTIVE, e.g. ng-controller
+// links the below HTML:
+//
+// index.html
+//
+<html>
+  <body ng-controller="StoreController">
+    ...
+  </body>
+</html>
+//
+// ... with the controller, which is a JS function in app.js:
+//
+function StoreController() {
+  alert('Welcome, Gregg!');
+}
+// Directives BIND the behaviour.
+//
+// WHAT ARE MODULES?
+//
+// Modules are where we write pieces of Angular app and how we keep code
+// encapsulated.  Makes code more readable and testable.  It's also where we
+// contain our dependencies.
+//
+// WHAT ARE CONTROLLERS?
+//
+// Controllers help get data onto our webpage.  Controlers are where we define
+// our app's BEHAVIOUR by defining functions and values.
